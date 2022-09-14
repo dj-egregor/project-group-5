7c8b88fd2f082f344b583169b0d7717a3c91bd86
dj-egregor <108410074+dj-egregor@users.noreply.github.com>
2022-09-14 10:35:13 +0000

    Deploying to gh-pages from @ dj-egregor/project-group-5@f65271723c874c91446f0fc5d35d2107cebbbcce 🚀

---
+++ order.bef1b1f0.js
@@ -0,0 +1,2 @@
+onload=function(){for(var t=document.links,e=0;e<t.length;e++)t[e].href==document.URL&&(t[e].style.cssText="-webkit-text-stroke: 1px #487996; -webkit-text-fill-color: transparent")};
+//# sourceMappingURL=order.bef1b1f0.js.map
