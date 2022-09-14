17eb5b10ffd0016ae2b641d196029893545ee6e0
dj-egregor <108410074+dj-egregor@users.noreply.github.com>
2022-09-14 15:25:52 +0000

    Deploying to gh-pages from @ dj-egregor/project-group-5@4fa3adae68cd3970d8461fe3e62b2eab60e82267 🚀

---
+++ index.abfb7c74.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementsByClassName("more-photo__item"),n=document.querySelector(".backdrop");console.log(e);for(var o=0;o<e.length;o++)e[o].addEventListener("click",t);function t(){n.classList.remove("is-hidden")}n.addEventListener("click",(function(){n.classList.add("is-hidden"),console.log()}))}();
+//# sourceMappingURL=index.abfb7c74.js.map
